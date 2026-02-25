b2dd220996e724aa8c4ae6955aecf5e638d86456
nerdymaxx <nerdymax84@gmail.com>
2026-02-25 13:26:00 +0100

    alx database setup

---
+++ task_6.sql
@@ -0,0 +1,8 @@
+INSERT INTO CUSTOMERS (customer_id, customer_name, email, address) 
+VALUES (2, "Blessing Malik", "bmalik@sandtech.com", "124 happiness ave.");
+
+INSERT INTO CUSTOMERS (customer_id, customer_name, email, address) 
+VALUE (3, "Obed Ehoneah", "eobed@sandtech.com", "125 happiness ave.");
+
+INSERT INTO CUSTOMERS (customer_id, customer_name, email, address) 
+VALUES (4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 happiness ave.");
